0efe971106c06ff82f3ec7c2c13cc302253669c0
agconture <agconture@teco.com.ar>
2020-08-21 14:18:15 -0300

    Removed folder from repository

--- airflow/basedatos/inventario.sql
+++
@@ -1,8 +0,0 @@
-create table inv_itf(ShelfName varchar (50), Hardware varchar (30), NetworkRole varchar (25), ShelfOperationalState varchar (10), UserLabel varchar (20), Bandwidth varchar (20), PortOperationalState varchar (20), Info1 varchar (500), Type varchar (20));
-
-create table par_inv_itf(ShelfName varchar (50), Hardware varchar (30), NetworkRole varchar (25), ShelfOperationalState varchar (10), UserLabel varchar (20), Bandwidth varchar (20), PortOperationalState varchar (20), Info1 varchar (500), Type varchar (20), concat varchar (90) );
-
-create table ne(shelfname varchar (50), interface varchar (20), portoperationalstate varchar (20), protocol varchar (20), info1 varchar (500), concat varchar (90));
-
-
-
