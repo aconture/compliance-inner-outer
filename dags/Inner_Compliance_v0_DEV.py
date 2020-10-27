b8c4054ae624e55ef72388dd0fbd54131c6e2eec
pdbute <51840612+pdbute@users.noreply.github.com>
2020-10-27 10:01:54 -0300

    dag de prueba

--- dags/Inner_Compliance_v0_DEV.py
+++ dags/Inner_Compliance_v0_DEV.py
@@ -888,7 +888,8 @@ _envia_mail1 = EmailOperator(
     subject="Compliance Inner&Outer - Resultado de Ejecucion {{ ds }}",
     #html_content="<h3> Esto es una prueba del envio de mail al finalizar la ejecucion del pipe </h3>",
     html_content=_cuerpo_mail(),
-    files=["/usr/local/airflow/reports/reporte.xlsx"],
+    #files=["/usr/local/airflow/reports/reporte.xlsx"],
+    files=["/home/pdbutera/Automation/v1.1/airflow/reports/reporte.xlsx"],
     dag=dag
 )
 
