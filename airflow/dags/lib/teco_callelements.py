f97189f9acb6e2d2a81d9185a271abfe1806c471
Adrian G Contureyuzon Colomes <AGConture@teco.com.ar>
2020-11-18 19:23:18 -0300

    elimino call_ansible en la libreria call_elements, para pasar a usarlo en el operador teco

--- airflow/dags/lib/teco_callelements.py
+++ airflow/dags/lib/teco_callelements.py
@@ -34,7 +34,6 @@ def scp_files(**context):
       none si exitoso
 
     """
-
     try:
         conn_id = context['connection']
         local_dir = context['local_dir']
@@ -64,140 +63,143 @@ def scp_files(**context):
 #####################################################################
 #####################################################################
 
-def call_ansible(**context):
-    
-    manual = """
+# def call_ansible(**context):
 
-    Ejecuta ansible en el directorio remoto. 
-    Si el directorio que contiene el resultado de ansible tiene la fecha actual, no se ejecuta ansible.
+#     print (':::ATENCION::: En breve dejaremos de usar esta funcion, y la reflejaremos en un operador.')
 
-    Args: 
-        pbook_dir: [Text]
-            El directorio donde esta ubicado el plabook a ejecutar.
+#     manual = """
 
-        playbook: [Text]
-            El nombre del playbook a ejecutar.
+#     Ejecuta ansible en el directorio remoto. 
+#     Si el directorio que contiene el resultado de ansible tiene la fecha actual, no se ejecuta ansible.
+
+#     Args: 
+#         pbook_dir: [Text]
+#             El directorio donde esta ubicado el plabook a ejecutar.
+
+#         playbook: [Text]
+#             El nombre del playbook a ejecutar.
         
-        [inventory]: [text]
-            Inventario a utilizar.
-            Sobreescribe el inventario configurado en el script de ansible.
+#         [inventory]: [text]
+#             Inventario a utilizar.
+#             Sobreescribe el inventario configurado en el script de ansible.
 
-        [init_output]: [text]
-            El path que se necesita limpiar antes de la ejecucion del playbook. Soporta wildcard (ej: *.txt).
+#         [init_output]: [text]
+#             El path que se necesita limpiar antes de la ejecucion del playbook. Soporta wildcard (ej: *.txt).
 
-        [mock]: [True (default) | False]
-            Evita ejecutar el playbook de ansible, usando datos mockeados.
-                True: usa información de NEs mockeada.
-                False: funcionamiento normal: ejecuta ansible para consultar la info en los NE
+#         [mock]: [True (default) | False]
+#             Evita ejecutar el playbook de ansible, usando datos mockeados.
+#                 True: usa información de NEs mockeada.
+#                 False: funcionamiento normal: ejecuta ansible para consultar la info en los NE
 
-        [mock_source]: [Text]
-            La ubicación de los archivos que contienen la información para realizar el mock. 
-            Soporta wildcard (ej: *.txt)
+#         [mock_source]: [Text]
+#             La ubicación de los archivos que contienen la información para realizar el mock. 
+#             Soporta wildcard (ej: *.txt)
 
-        [mock_dest]: [Text]
-            El directorio de destino de los archivos que contienen la información para realizar el mock. 
-            Es el directorio de trabajo de donde toma la información el caso de uso.
+#         [mock_dest]: [Text]
+#             El directorio de destino de los archivos que contienen la información para realizar el mock. 
+#             Es el directorio de trabajo de donde toma la información el caso de uso.
 
-    """
-    try:
-        pbook_dir = context['pbook_dir']
-        playbook = context['playbook']
-    except:
-        logging.error ('\n\n:::! Error - Falta un argumento de llamada a esta funcion.')
-        logging.error (manual)
-        return -1
+#     """
+    
+#     try:
+#         pbook_dir = context['pbook_dir']
+#         playbook = context['playbook']
+#     except:
+#         logging.error ('\n\n:::! Error - Falta un argumento de llamada a esta funcion.')
+#         logging.error (manual)
+#         return -1
 
-    try:
-        mock = context['mock']
-    except:
-        mock = True
+#     try:
+#         mock = context['mock']
+#     except:
+#         mock = True
 
-    if mock:
-        #os.system ('rm /usr/local/ansible/mejoras_cu1/interfaces/*.txt; cp -p /usr/local/ansible/mejoras_cu1/interfaces_mock/*.txt /usr/local/ansible/mejoras_cu1/interfaces/')
-        try:
-            init_output = context['init_output']
-            mock_source = context['mock_source']
-            mock_dest = context['mock_dest']
-            os.system ('rm {0}'.format(init_output))
-            os.system ('cp -p {0} {1}'.format(mock_source, mock_dest))
-            logging.info ('\n\n:::Salimos por mockeo...\n\n')
-        except:
-            logging.error ('Error al recuperar o ejecutar un parametro de mockeo. Ver manual:\n {0}'.format(manual))
-            raise ValueError ('Error en mockeo')
-        #os.system ('cp -p /usr/local/ansible/mejoras_cu1/interfaces_mock/*.txt /usr/local/ansible/mejoras_cu1/interfaces/')
+#     if mock:
+#         #os.system ('rm /usr/local/ansible/mejoras_cu1/interfaces/*.txt; cp -p /usr/local/ansible/mejoras_cu1/interfaces_mock/*.txt /usr/local/ansible/mejoras_cu1/interfaces/')
+#         try:
+#             init_output = context['init_output']
+#             mock_source = context['mock_source']
+#             mock_dest = context['mock_dest']
+#             os.system ('rm {0}'.format(init_output))
+#             os.system ('cp -p {0} {1}'.format(mock_source, mock_dest))
+#             logging.info ('\n\n:::Salimos por mockeo...\n\n')
+#         except:
+#             logging.error ('Error al recuperar o ejecutar un parametro de mockeo. Ver manual:\n {0}'.format(manual))
+#             raise ValueError ('Error en mockeo')
+#         #os.system ('cp -p /usr/local/ansible/mejoras_cu1/interfaces_mock/*.txt /usr/local/ansible/mejoras_cu1/interfaces/')
 
-        return
+#         return
 
-    update = _check_vigencia (**context)
-    if (update):
-        logging.info (':::La base de Network Element tiene fecha en el pasado. Ejecutando Ansible para actualizar.')
-        try:
-            #os.system ('rm /usr/local/ansible/mejoras_cu1/interfaces/*.txt; cd /usr/local/ansible/mejoras_cu1/yaml; ansible-playbook main.yaml')
-            try:
-                init_output = context['init_output']
-                os.system ('rm {0}'.format(init_output))
-            except:
-                pass
+#     update = _check_vigencia (**context)
+#     if (update):
+#         logging.info (':::La base de Network Element tiene fecha en el pasado. Ejecutando Ansible para actualizar.')
+#         try:
+#             #os.system ('rm /usr/local/ansible/mejoras_cu1/interfaces/*.txt; cd /usr/local/ansible/mejoras_cu1/yaml; ansible-playbook main.yaml')
+#             try:
+#                 init_output = context['init_output']
+#                 os.system ('rm {0}'.format(init_output))
+#             except:
+#                 pass
 
-            try:
-                inventory = context['inventory']
-                r = ansible_runner.run(private_data_dir=pbook_dir, playbook=playbook, inventory=inventory)
-            except:
-                r = ansible_runner.run(private_data_dir=pbook_dir, playbook=playbook)
+#             try:
+#                 inventory = context['inventory']
+#                 r = ansible_runner.run(private_data_dir=pbook_dir, playbook=playbook, inventory=inventory)
+#             except:
+#                 r = ansible_runner.run(private_data_dir=pbook_dir, playbook=playbook)
 
-            print("{}: {}".format(r.status, r.rc))
-            # successful: 0
-            for each_host_event in r.events:
-                print(each_host_event['event'])
-            print("Final status:")
-            print("====================================")
-            print("====================================")
-            print("La salida de ansible es: ",r.stats)
+#             print("{}: {}".format(r.status, r.rc))
+#             # successful: 0
+#             for each_host_event in r.events:
+#                 print(each_host_event['event'])
+#             print("Final status:")
+#             print("====================================")
+#             print("====================================")
+#             print("La salida de ansible es: ",r.stats)
 
-            print("====================================")
-            print("====================================")
-            print("====================================")
-            print("====================================")
+#             print("====================================")
+#             print("====================================")
+#             print("====================================")
+#             print("====================================")
 
-            # if len(LansibleFairlure)==0:
-            #     logging.info ('\n\n:::! Ansible ejecutado correctamente para todos los elementos de red\n')
+#             # if len(LansibleFairlure)==0:
+#             #     logging.info ('\n\n:::! Ansible ejecutado correctamente para todos los elementos de red\n')
 
-            # LansibleFairlure=[]
-            # if ansibleprintfailures is None:
-            #     logging.info ('\n\n:::! Ansible ejecutado correctamente para todos los elementos de red\n')
-            # else:
-            #     for ansibleprintfailures in ansibleprint_raw:
-            #         print ("Las fallas de ejecución fueron las siguientes: ",ansibleprintfailures)
-            #         #FUNCION BORRADO
-            #         #ansiblefailures = teco_db.insert_ansible_failures(ansibleprintfailures)
-            #         #print ("Salida insert DB: ",ansiblefailures)
-            #         LansibleFairlure.append (ansibleprintfailures)
+#             # LansibleFairlure=[]
+#             # if ansibleprintfailures is None:
+#             #     logging.info ('\n\n:::! Ansible ejecutado correctamente para todos los elementos de red\n')
+#             # else:
+#             #     for ansibleprintfailures in ansibleprint_raw:
+#             #         print ("Las fallas de ejecución fueron las siguientes: ",ansibleprintfailures)
+#             #         #FUNCION BORRADO
+#             #         #ansiblefailures = teco_db.insert_ansible_failures(ansibleprintfailures)
+#             #         #print ("Salida insert DB: ",ansiblefailures)
+#             #         LansibleFairlure.append (ansibleprintfailures)
 
-            # logging.info (':::Elementos fallados'.format(LansibleFairlure))
+#             # logging.info (':::Elementos fallados'.format(LansibleFairlure))
 
 
-        except:
-            logging.error ('\n\n:::! Problema en la conexión a la Red.\n')
-            raise ValueError ('Error en la conexión a la Red')
-            print("====================================")
-            print("====================================")
-            print("La salida de ansible es: ",r.stats)  
-            return -1
+#         except:
+#             logging.error ('\n\n:::! Problema en la conexión a la Red.\n')
+#             raise ValueError ('Error en la conexión a la Red')
+#             print("====================================")
+#             print("====================================")
+#             print("La salida de ansible es: ",r.stats)  
+#             return -1
 
-        finally:
-            ansibleprint_raw = r.stats["failures"]
-            print ('ESTOY EJECUTANDO ANSIBLE::::::::::::::::::::::::::')
-            print ('EL ansibleprint_raw es ', ansibleprint_raw)
+#         finally:
+#             ansibleprint_raw = r.stats["failures"]
+#             print ('ESTOY EJECUTANDO ANSIBLE::::::::::::::::::::::::::')
+#             print ('EL ansibleprint_raw es ', ansibleprint_raw)
 
-            LansibleFairlure = []
-            for fairlureItem in ansibleprint_raw:
-                print (':::::::::::::::::::',fairlureItem)
-                LansibleFairlure.append (fairlureItem)
-            logging.info (':::Elementos fallados {0}'.format(LansibleFairlure))
-            insert_ansible_failures(LansibleFairlure)
+#             LansibleFairlure = []
+#             for fairlureItem in ansibleprint_raw:
+#                 print (':::::::::::::::::::',fairlureItem)
+#                 LansibleFairlure.append (fairlureItem)
+#             logging.info (':::Elementos fallados {0}'.format(LansibleFairlure))
+#             insert_ansible_failures(LansibleFairlure)
 
-    else:
-        logging.info (':::Base de datos de NE ya actualizada, no es necesario actualizar.')
+#     else:
+#         logging.info (':::Base de datos de NE ya actualizada, no es necesario actualizar.')
 
 
 #####################################################################
